77dd679059369279bd191d325718eb2666d65ae6
Camargo Castro <camargo.castro@icloud.com>
2024-12-02 04:13:38 +0100

    add desktops app

---
+++ desktop-apps.sh
@@ -0,0 +1,34 @@
+# Browser = Firefox
+open https://www.mozilla.org/en-US/firefox/mac/
+
+# Integrated development environment - Visual studio
+open https://code.visualstudio.com/download
+
+# Console = Iterm2
+open https://iterm2.com/downloads.html
+
+# Docker
+open https://docs.docker.com/desktop/setup/install/mac-install/
+
+# Password manager = 1password
+open https://1password.com/downloads/mac
+
+
+# communication
+## Zoom
+open https://zoom.us/download?os=mac
+## Telegram
+open https://desktop.telegram.org/
+## Whatsapp
+open https://www.whatsapp.com/download
+
+# Economies
+open https://www.tradingview.com/desktop/
+
+# VPN
+open https://www.wireguard.com/install/
+
+# Music
+open https://www.spotify.com/de-en/download/other/
+
+
